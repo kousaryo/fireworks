9f133b96285adf7cb55bc172c18618bae505fe37
kousaryo <33143969+kousaryo@users.noreply.github.com>
2017-12-02 10:01:16 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var fireworks = [];
+var gravity;
+var mic;
+var particle = [];
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    mic = new p5.AudioIn();
+    mic.start();
+    colorMode(HSB);
+    gravity = createVector(0, 0.2);
+    stroke(255);
+    strokeWeight(5);
+    background(0);
+}
+
+function draw() {
+    colorMode(RGB);
+    background(0,0,0,25);
+    var vol = mic.getLevel();
+    if (vol > 0.3) {
+        fireworks.push(new Firework());
+    }
+    for (var i = fireworks.length - 1; i >= 0; i--) {
+        fireworks[i].update();
+        fireworks[i].show();
+        if (fireworks[i].done()) {
+            fireworks.splice(i, 1);
+        }
+    }
+    if(fireworks.length > 0){
+       console.log(particle.done);
+}
+}
